d197ab332f1966da13d3354ceb78d1726b9b2a18
maxprilutskiy <maks.prilutskiy@gmail.com>
2023-08-16 13:51:03 +0200

    feat: added twitter redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    // redirect all traffic to https://twitter.com/MaxPrilutskiy
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://twitter.com/MaxPrilutskiy',
+        permanent: false,
+      },
+    ]
+  },
   experimental: {
     appDir: true,
   },
