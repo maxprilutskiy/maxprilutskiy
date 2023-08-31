b1cea88225ddc4b9edfec196c79330a1fc4b01ea
Max Prilutskiy <5614659+maxprilutskiy@users.noreply.github.com>
2023-08-31 12:12:53 +0200

    feat: added ph link

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     // redirect all traffic to https://twitter.com/MaxPrilutskiy
     return [
       {
+        source: '/r/product-hunt',
+        destination: 'https://aabhashk.notion.site/aabhashk/Product-Hunt-Launch-Playbook-by-AabhashK-306bf17938ee4c28aa1482080b3b0064',
+        permanent: false,
+      },
+      {
         source: '/:path*',
         destination: 'https://twitter.com/MaxPrilutskiy',
         permanent: false,
